fab9d1becabe66790b72895a8dc3f798797fe113
ryutah <ryuta0221@gmail.com>
2018-03-16 18:17:18 +0900

    Add: Sign status control, verify email

--- front/src/app.js
+++ front/src/app.js
@@ -7,10 +7,72 @@ import * as firebaseui from "./firebaseui/npm__ja";
 
 firebase.initializeApp(config);
 
-const uiConfig = {
-  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
-  credentialHelper: firebaseui.auth.CredentialHelper.NONE
-};
+{
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start("#firebaseui-auth-container", uiConfig);
+  const signArea = document.querySelector("#sign-area");
+
+  const showEmailVerifyComponents = () => {
+    const verifyArea = document.querySelector("#email-verify-area");
+    const verify = document.createElement("button");
+    verify.addEventListener("click", e => {
+      e.preventDefault();
+
+      firebase
+        .auth()
+        .currentUser.sendEmailVerification({ url: "http://localhost:8080/" })
+        .then(() => alert("メールを送ったぜ"))
+        .catch(console.error);
+    });
+    verify.innerText = "アドレス確認メール送信";
+    verifyArea.appendChild(verify);
+  };
+
+  const showLogout = () => {
+    const logoutBtn = document.createElement("button");
+    logoutBtn.id = "logout";
+    logoutBtn.innerText = "Logout";
+    logoutBtn.addEventListener("click", e => {
+      e.preventDefault();
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {})
+        .catch(console.error);
+    });
+
+    signArea.appendChild(logoutBtn);
+  };
+  const hideLogout = () => {
+    const logoutBtn = document.querySelector("#logout");
+    if (logoutBtn) signArea.removeChild(logoutBtn);
+  };
+
+  const isVerifyUserEmail = user => {
+    const provider = user.providerData.filter(p => p.providerId === "password");
+    if (provider.length === 0) {
+      return true;
+    }
+    return user.emailVerified;
+  };
+
+  const uiConfig = {
+    callbacks: {
+      signInSuccess: () => false
+    },
+    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE
+  };
+
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      showLogout();
+      if (!isVerifyUserEmail(user)) {
+        showEmailVerifyComponents();
+      }
+    } else {
+      hideLogout();
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+  });
+}
