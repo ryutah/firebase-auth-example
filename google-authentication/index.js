cd9fbc35820afc4b728dce3a350955bd0a4d1eaf
ryutah <ryuta0221@gmail.com>
2018-07-04 15:09:37 +0900

    Add: Google login example

---
+++ google-authentication/index.js
@@ -0,0 +1,52 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: `${process.env.PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.PROJECT_ID,
+};
+
+firebase.initializeApp(config);
+
+window.firebase = firebase;
+
+document.querySelector("#login").addEventListener("click", () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    redirect_uri: "http://localhost:1234",
+  });
+  firebase
+    .auth()
+    .signInWithRedirect(provider)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch(console.error);
+});
+
+document.querySelector("#logout").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Click logout");
+
+  firebase
+    .auth()
+    .signOut()
+    .then(() => console.log("Success sign out"))
+    .catch(console.error);
+});
+
+firebase.auth().onAuthStateChanged((usr) => {
+  if (usr) {
+    console.log(usr);
+  }
+  window.currentUser = usr;
+});
+
+firebase
+  .auth()
+  .getRedirectResult()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch(console.error);
